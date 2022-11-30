e3999a0dde04223a0a4d0aafa2d2e06d0643094e
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-11-30 20:28:26 +0100

    added server start setting

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+const formatLogger = app.get('env') === 'development' ? 'dev' : 'short';
+
+app.use(logger(formatLogger));
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+app.use((err, req, res, next) => {
+  res.status(500).json({ message: err.message });
+});
+
+module.exports = app;
--- bin/server.js
+++ bin/server.js
@@ -1,20 +1,5 @@
-const express = require('express');
-const serverLogMiddleware = require('../middleware/server.log');
+const app = require('../app');
 
-const backEnd = require('./../backEnd');
+const { PORT = 8081 } = process.env;
 
-const reviews = backEnd.reviews;
-
-const app = express();
-
-app.use(serverLogMiddleware);
-
-app.get('/reviews', (req, res) => {
-  res.json({
-    status: 'success',
-    code: 200,
-    data: { result: reviews },
-  });
-});
-
-app.listen(3000, () => console.log('listening on port 3000'));
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
