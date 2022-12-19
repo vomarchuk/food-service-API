370c55ef4021cdc83a2d5aab8a564a70e2b293cb
Volodymyr Marchuk <mr.volodymyr.marchuk@gmail.com>
2022-12-19 20:18:14 +0100

    change connectDatabase

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
-const { DB_HOST, DB_NAME, PORT } = process.env;
+const { DB_HOST, DB_NAME, PORT, NODE_ENV } = process.env;
 
 module.exports = {
+  PORT,
   DB_HOST,
   DB_NAME,
-  PORT,
+  NODE_ENV,
 };
